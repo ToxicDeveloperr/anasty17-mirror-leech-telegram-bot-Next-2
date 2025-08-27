c68266ddb6786066625369679b9d82e1d7b24855
ToxicDeveloperr <154732531+ToxicDeveloperr@users.noreply.github.com>
2025-08-27 08:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TELEGRAM_HASH = "354e1dd8e1e3041ee2145196da8d6aac"
 TG_PROXY = {}
 USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
-AUTHORIZED_CHATS = "-1001944752172"
+AUTHORIZED_CHATS = ""
 SUDO_USERS = "7210955390"
 DATABASE_URL = "mongodb+srv://tejaschavan1110:cSxC44OLfIPxcXxp@cluster0.iu0f4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 STATUS_LIMIT = 4
